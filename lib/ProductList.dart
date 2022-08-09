0d4ae65930fdaa533ab41a4121dd116c49417978
sajibsaha4647 <sksaha4648@gmail.com>
2022-08-09 11:34:57 +0600

    updated

---
+++ lib/ProductList.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class ProductList extends StatefulWidget {
+  const ProductList({Key? key}) : super(key: key);
+
+  @override
+  State<ProductList> createState() => _ProductListState();
+}
+
+class _ProductListState extends State<ProductList> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
